f3647f6de19f8045e6248b4fbea434457ff89cd6
YusukeNaito08 <yusuke0208na@gmail.com>
2021-01-15 09:09:02 +0000

    [Fix]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,9 @@ def after_sign_in_path_for(resources)
   user_path(current_user)
 end
 
+def after_sign_up_path_for(resources)
+   user_path(current_user)
+end
 
 protected
 def configure_permitted_parameters
